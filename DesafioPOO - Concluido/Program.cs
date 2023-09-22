8406e28d6db1358b1757cc1bbd440e2c319a5600
Pedro Henrique <139706246+PedroHnriqqe@users.noreply.github.com>
2023-09-22 17:36:56 -0300

    Desafio concluído
    Neste projeto os alunos tem a missão de criar o código a partir do diagrama disponibilizado pelo professor, ótima maneira de aplicar os conhecimentos adquiridos na formação.

---
+++ DesafioPOO - Concluido/Program.cs
@@ -0,0 +1,24 @@
+﻿using DesafioPOO.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioPOO
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Iphone iphone = new Iphone("1199999999", "Iphone xs", "3242314132", 120);
+
+            iphone.InstalarAplicativo("Whatsapp");
+            iphone.Ligar();
+
+            Nokia nokia = new Nokia("1144322222", "Nokita Tijolao i5", "2343125123", 65);
+            nokia.InstalarAplicativo("Telegram");
+            nokia.Ligar();
+        }
+    }
+}
